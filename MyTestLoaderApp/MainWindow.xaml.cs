a26722852185334236d63b707b1a5c1e99594c7f
Roman <Roman@LENOVO>
2017-10-24 12:54:09 +0300

    first commit

--- MyTestLoaderApp/MainWindow.xaml.cs
+++ MyTestLoaderApp/MainWindow.xaml.cs
@@ -24,5 +24,24 @@ namespace MyTestLoaderApp
         {
             InitializeComponent();
         }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double value = ((Slider)sender).Value;
+            string round_value = Math.Round(((Slider)sender).Value, 2).ToString();
+
+            if (value == 0 || value == 100)
+            {
+                bar.Value = value;
+                textBlock1.Text = round_value + ".00%";
+                textBlock2.Text = round_value + ".00%";
+            }
+            else
+            {
+                bar.Value = value;
+                textBlock1.Text = round_value + "%";
+                textBlock2.Text = round_value + "%";
+            }
+        }
     }
 }
